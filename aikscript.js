d32367edd346710ccf6d06d895715a3f424f5f35
Aik <ikogweassurance85@gmail.com>
2022-12-19 06:04:21 +0100

    the number keys, del and reset are working, time to work on the operators

--- aikscript.js
+++ aikscript.js
@@ -1,17 +1,16 @@
-const buttons = $(".row>button");
-const buttons2 = $(".row2>button");
+const buttons = $("div button");
 const display = $("#result_value");
 display.html('') 
 
 for(let i = 0; i < buttons.length; i++){
     buttons[i].addEventListener("click", () => {
-        display.html(display.html() + buttons[i].innerHTML);
-        // console.log(display.innerHTML)
+        if(buttons[i].innerHTML == "DEL"){
+            let number = display.html();
+            display.html(number.slice(0, -1));
+        }else if(buttons[i].innerHTML == "RESET"){
+            display.html('');
+        } else{
+            display.html(display.html() + buttons[i].innerHTML);
+        }
     });
 }
-
-// for(let i = 0; i < buttons2.length; i++){
-//     buttons2[i].addEventListener("click", () => {
-//         console.log(buttons2[i].innerHTML);
-//     });
-// }
